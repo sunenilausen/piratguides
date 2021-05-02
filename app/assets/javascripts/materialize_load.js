2466603cbecb8700df3638b1ddb512fdd4087a9c
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2021-05-02 19:24:49 +0200

    Fix for double dropdown click event

--- app/assets/javascripts/materialize_load.js
+++ app/assets/javascripts/materialize_load.js
@@ -1,3 +1,7 @@
 $(document).on('turbolinks:load', function() {
   window.materializeForm.init()
 });
+
+$(document).on("click", ".select-wrapper", function (event) {
+  event.stopPropagation();
+});
